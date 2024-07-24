7a9dbc3013fa15af01a40befaf28ded25856f731
Antony Angeli <antony.angeli@Antonys-Laptop.local>
2024-07-24 17:14:35 +0100

    add customer class

--- src/main/scala/Utils/POSError.scala
+++ src/main/scala/Utils/POSError.scala
@@ -6,4 +6,8 @@ object POSError{
   case class InvalidItemType(message:String) extends POSError
   case class InvalidStamp(message:String) extends POSError
   case class InsufficientStamps(message:String) extends POSError
+  case class InvalidAge(message:String) extends POSError
+  case class AlreadyHasCard(message:String) extends POSError
+  case class InvalidMinPurchases(message:String) extends POSError
+  case class InvalidMinSpendTotal(message:String) extends POSError
 }
