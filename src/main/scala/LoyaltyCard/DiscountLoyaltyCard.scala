544ed520093fd97e1a0bfee87f8ecd261a740455
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-24 17:31:34 +0100

    created methods for var

--- src/main/scala/LoyaltyCard/DiscountLoyaltyCard.scala
+++ src/main/scala/LoyaltyCard/DiscountLoyaltyCard.scala
@@ -1,15 +1,29 @@
 package LoyaltyCard
 
 import Utils.POSError
+import Utils.POSError.InvalidStamp
 
 import java.time.LocalDate
+import scala.collection.View.Empty
 
-case class DiscountLoyaltyCard(customerStars:Option[Int], customerStamps:Option[List[LocalDate]]) extends LoyaltyCard {
-  val maxStars:Int = 8
+case class DiscountLoyaltyCard(customerStars: Option[List[LocalDate]]) extends LoyaltyCard {
+  val maxStars: Int = 8
+  private var currentCustomerStars = customerStars match {
+    case Some(stars) => if (stars.length <= 8) stars else List()
+    case None => List()
+  }
 
-  def addStar(orderPrice:Double):Either[POSError, Int] = {
+
+  def addStar(orderPrice: Double): Either[POSError, Int] = {
+    if (orderPrice > 20) {
+      if (currentCustomerStars.length == 8) Left(InvalidStamp("Star Cannot be added"))
+      else if (currentCustomerStars.length < 8) Right()
+    }
+
+  }
+
+  def getDiscount(orderPrice: Double): Double = {
     ???
   }
-  def getDiscount(orderPrice:Double):Double = {???}
 
 }
