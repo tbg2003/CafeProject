6c16ea33bb74c98ba94481f0ede13f6c79844bcd
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-07-25 11:33:11 +0100

    defined bill methods

--- src/main/scala/Bill/Bill.scala
+++ src/main/scala/Bill/Bill.scala
@@ -39,11 +39,16 @@ case class Bill(order:List[MenuItem],
   }
 
   def applyServiceCharge():Double = {
-    // apply discounts from loyalty card
+    // if drinks loyaly then bill = apply drinks
+    // if discount loyal = apply discount loyalty
+    // else = sum bill
+    // get service charge and add
+    // if extra tip then add
+    // return total
     ???
   }
 
-  def applyServiceWithDrinksLoyalty():Double = {
+  def applyDrinksLoyalty():Double = {
     // sum up items
     // if cold drink or hot drink in item types
     //    if get free drink
@@ -52,7 +57,7 @@ case class Bill(order:List[MenuItem],
     ???
   }
 
-  def applyServiceWithDiscountLoyalty():Double = {
+  def applyDiscountLoyalty():Double = {
     // sum up items - sum up bill specials
     // apply discount to bill
     // + sum of bill specials
@@ -61,5 +66,14 @@ case class Bill(order:List[MenuItem],
     ???
   }
 
-  def getServiceCharge():Int={}
+  def getServiceCharge():ServiceChargeType={
+    // if pay service then
+      // get item type list
+      // if contains Specials = Special service charge
+      // else if Hot Food = HFSC
+      // else if Hot Drinks HDSC
+      // else NOSC
+    // else NOSC
+    ???
+  }
 }
