a4607d8a34d662a14d2ad07609f098d46f380afd
Roshan Gautam <roshangautam2052@gmail.com>
2024-07-25 11:08:56 +0100

    completed all test cases

--- src/main/scala/LoyaltyCard/DiscountLoyaltyCard.scala
+++ src/main/scala/LoyaltyCard/DiscountLoyaltyCard.scala
@@ -7,35 +7,35 @@ import java.time.LocalDate
 
 case class DiscountLoyaltyCard(customerStars: Option[List[LocalDate]]) extends LoyaltyCard {
   val maxStars: Int = 8
-  private val currentCustomerStars = customerStars match {
-    case Some(stars) => if (stars.nonEmpty && stars.length<=maxStars) stars else List()
+  private var currentCustomerStars = customerStars match {
+    case Some(stars) => if (stars.nonEmpty && stars.length <= maxStars) stars else List()
     case None => List()
   }
 
-  def getCustomersCard(): List[LocalDate] ={
+  def getCustomersCard(): List[LocalDate] = {
     currentCustomerStars
   }
 
 
-  def addStar(orderPrice: Double): Either[POSError, String] = {
-    if (orderPrice >=20){
-       if(customerStars.contains(Some(LocalDate.now))){
+  def addStar(orderPrice: Double): Either[POSError, Option[List[LocalDate]]] = {
+    if (orderPrice >= 20) {
+      if (currentCustomerStars.contains(LocalDate.now)) {
         Left(InvalidStamp("Stars Already Added Stamped today"))
       }
       else if (currentCustomerStars.length >= maxStars) Left(InvalidStamp("No more stars can be added"))
-      else{
-        currentCustomerStars:+ LocalDate.now()
-        Right("One star Added Successfully!!!")
+      else {
+        currentCustomerStars = currentCustomerStars :+ LocalDate.now()
+        Right(Some(currentCustomerStars))
       }
     }
-    else{
+    else {
       Left(InvalidStamp("Purchase price is less than 20 pounds"))
     }
 
   }
 
   def getDiscount(): Double = {
-    currentCustomerStars.length*0.02
+    currentCustomerStars.length * 0.02
   }
 
 }
